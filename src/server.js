969147d7700d0cbb5c92a34a6c6134d2126da8f0
Guilherme Zordan <guilhermezordan@outlook.com>
2022-07-28 09:34:28 -0300

    fix: package.json cleanup

--- src/server.js
+++ src/server.js
@@ -2,13 +2,8 @@ import Fastify from 'fastify'
 import checkRoutes from './routes/checks.js'
 import formbody from '@fastify/formbody'
 
-export default function buildServer(config) {
-  const fastify = Fastify({
-    logger: {
-      level: config.LOG_LEVEL,
-      prettyPrint: config.PRETTY_PRINT
-    }
-  })
+export default function buildServer() {
+  const fastify = Fastify()
 
   fastify.register(formbody)
   fastify.register(checkRoutes)
