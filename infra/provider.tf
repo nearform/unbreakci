424ce201d9b3c9681cfc337298c776d59fe84aeb
Diego Cunha <diego.cunha@nearform.com>
2022-07-29 20:38:29 -0300

    Adjusted the terraform fmt

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 provider "google-beta" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
