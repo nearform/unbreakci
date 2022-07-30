153d94f5ab0be27a86d66c027b177a7f4a657d3f
Diego Cunha <diego.cunha@nearform.com>
2022-07-29 21:05:09 -0300

    Adjusted the pipeline.

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,13 +8,15 @@ terraform {
 }
 
 provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
+  credentials = file("key.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
 
 provider "google-beta" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
+  credentials = file("key.json")
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
